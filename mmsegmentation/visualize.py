f2c26cf3ab4c49aced87074404708c1b89ee5ec7
Chaerin <c282828177@gmail.com>
2024-01-09 06:41:31 -0500

    folk test

--- mmsegmentation/visualize.py
+++ mmsegmentation/visualize.py
@@ -51,13 +51,13 @@ def visualize_twohands_obj2(img, seg_result, alpha = 0.4):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--mode", default='twohands_obj1', type=str, help='options: twohands, cb, twohands_obj1, twohands_obj2')
-    parser.add_argument("--img_dir", default='/mnt/session_space/home/lingzzha/EgoHOS/testimages/images', type=str)
-    parser.add_argument("--twohands_dir", default='/mnt/session_space/home/lingzzha/EgoHOS/testimages/pred_twohands', type=str)
+    parser.add_argument("--mode", default='twohands', type=str, help='options: twohands, cb, twohands_obj1, twohands_obj2')
+    parser.add_argument("--img_dir", default='../myimages/images', type=str)
+    parser.add_argument("--twohands_dir", default='../myimages/pred_twohands', type=str)
     parser.add_argument("--cb_dir", default='/mnt/session_space/home/lingzzha/EgoHOS/testimages/pred_cb', type=str)
     parser.add_argument("--obj1_dir", default='/mnt/session_space/home/lingzzha/EgoHOS/testimages/pred_obj1', type=str)
     parser.add_argument("--obj2_dir", default='/mnt/session_space/home/lingzzha/EgoHOS/testimages/pred_obj2', type=str)
-    parser.add_argument("--vis_dir", default='/mnt/session_space/home/lingzzha/EgoHOS/testimages/pred_obj1_vis', type=str)
+    parser.add_argument("--vis_dir", default='../myimages/pred_twohands_vis', type=str)
     args = parser.parse_args()
 
     os.makedirs(args.vis_dir, exist_ok = True)
@@ -69,12 +69,14 @@ if __name__ == '__main__':
         if args.mode == 'twohands':
             twohands = np.array(Image.open(os.path.join(args.twohands_dir, fname + '.png')))
             twohands_vis = visualize_twohands(img, twohands)
-            imsave(os.path.join(args.vis_dir, fname + '.jpg'), twohands_vis)
+            assert twohands_vis.max() > 2
+            imsave(os.path.join(args.vis_dir, fname + '.jpg'), twohands_vis.astype(np.uint8))
 
         elif args.mode == 'cb':
             cb = np.array(Image.open(os.path.join(args.cb_dir, fname + '.png')))
             cb_vis = visualize_cb(img, cb)
-            imsave(os.path.join(args.vis_dir, fname + '.jpg'), cb_vis)       
+            assert cb_vis.max() > 2
+            imsave(os.path.join(args.vis_dir, fname + '.jpg'), cb_vis.astype(np.uint8))       
 
         elif args.mode == 'twohands_obj1':
             twohands = np.array(Image.open(os.path.join(args.twohands_dir, fname + '.png')))
@@ -84,7 +86,8 @@ if __name__ == '__main__':
             twohands_obj1[obj1 == 2] = 4
             twohands_obj1[obj1 == 3] = 5
             twohands_obj1_vis = visualize_twohands_obj1(img, twohands_obj1)
-            imsave(os.path.join(args.vis_dir, fname + '.jpg'), twohands_obj1_vis)
+            assert twohands_obj1_vis.max() > 2
+            imsave(os.path.join(args.vis_dir, fname + '.jpg'), twohands_obj1_vis.astype(np.uint8))
 
         elif args.mode == 'twohands_obj2':
             twohands = np.array(Image.open(os.path.join(args.twohands_dir, fname + '.png')))
@@ -97,7 +100,8 @@ if __name__ == '__main__':
             twohands_obj2[obj2 == 5] = 7
             twohands_obj2[obj2 == 6] = 8
             twohands_obj2_vis = visualize_twohands_obj2(img, twohands_obj2)
-            imsave(os.path.join(args.vis_dir, fname + '.jpg'), twohands_obj2_vis)
+            assert twohands_obj2_vis.max() > 2
+            imsave(os.path.join(args.vis_dir, fname + '.jpg'), twohands_obj2_vis.astype(np.uint8))
 
         else:
             raise NotImplementedError
